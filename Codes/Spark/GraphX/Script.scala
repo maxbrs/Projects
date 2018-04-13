0e44c2bbb1aed3ca707efa7e2fb9c98dfaf1d3a5
Maxime BRIENS <maxbriens@gmail.com>
2018-04-13 14:18:27 +0200

    Edit first GraphX tryout

--- Codes/Spark/GraphX/Script.scala
+++ Codes/Spark/GraphX/Script.scala
@@ -4,11 +4,35 @@ import org.apache.spark.util.IntParam
 import org.apache.spark.graphx._
 import org.apache.spark.graphx.util.GraphGenerators 
 
-val vertexRDD: RDD[(Long, (String, Int))] = sc.parallelize(vertexArray)
-val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edgeArray)
-val graph: Graph[(String, Int), Int] = Graph(vertexRDD, edgeRDD)
-graph.vertices.filter { case (id, (name, age)) => age > 30 }
-.collect.foreach { case (id, (name, age)) => println(s"$name is $age")}
+var nodeArray: Array[(Long, (String, Int))] = Array(
+    (1, ("Alice", 28)),
+    (2, ("Bob", 27)),
+    (3, ("Charlie", 65)),
+    (4, ("David", 42)),
+    (5, ("Ed", 55)),
+    (6, ("Fran", 50))
+)
+var verticeArray: Array[Edge[Int]] = Array(
+    Edge(2, 1, 7),
+    Edge(2, 4, 2),
+    Edge(3, 2, 4),
+    Edge(3, 6, 3),
+    Edge(4, 1, 1),
+    Edge(5, 2, 2),
+    Edge(5, 3, 8),
+    Edge(5, 6, 3)
+)
+
+var nodeRDD: RDD[(Long, (String, Int))] = sc.parallelize(nodeArray)
+var verticeRDD: RDD[Edge[Int]] = sc.parallelize(verticeArray)
+var graph: Graph[(String, Int), Int] = Graph(nodeRDD, verticeRDD)
+graph.cache()
+
+graph
+.vertices
+.filter { case (id, (name, age)) => age > 30 }
+.collect
+.foreach { case (id, (name, age)) => println(s"$name is $age")}
 
 for (triplet <- graph.triplets.collect) {
     println(s"${triplet.srcAttr._1} likes ${triplet.dstAttr._1}")
