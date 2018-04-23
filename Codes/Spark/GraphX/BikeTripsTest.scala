5e0b628c3f4fb3d62ef2b5ef1e0a1284f82038f3
Maxime BRIENS <maxbriens@gmail.com>
2018-04-23 15:29:48 +0200

    Update GraphX (bike trips)

--- Codes/Spark/GraphX/BikeTripsTest.scala
+++ Codes/Spark/GraphX/BikeTripsTest.scala
@@ -30,21 +30,21 @@ val tripData = bikeTrip.
     join(stationsData, bikeTrip("End Station") === stationsData("name")).
     withColumnRenamed("station_id", "end_station_id").
     drop("name")
-//tripData.show(10)
+// tripData.show(10)
 
 var tripVertices: RDD[Edge[Long]] = tripData.
     select("start_station_id", "end_station_id").
     rdd.
     map(x => (x, 1)).
     reduceByKey((a, b) => a + b).
-    distinct().
+    // distinct().
     map(x => {
         val start = x._1(0).asInstanceOf[Number].longValue
-        val end = x._1(0).asInstanceOf[Number].longValue
+        val end = x._1(1).asInstanceOf[Number].longValue
         Edge(start, end, x._2)
     })
-// tripRDD.count()
-// tripRDD.foreach(x => println(x))
+// tripVertices.count()
+// tripVertices.foreach(x => println(x))
 
 
 var stationNodes: RDD[(VertexId, (String, List[Double], Int, String, String))] = 
@@ -61,21 +61,36 @@ var stationNodes: RDD[(VertexId, (String, List[Double], Int, String, String))] =
         (id, (name, gps, dockCount, city, date))
     }).
     sortBy(x => x._1, ascending = true)
-// stationRDD.count()
-// stationRDD.foreach(x => println(x))
+// stationNodes.count()
+// stationNodes.foreach(x => println(x))
 
-var graph: Graph[(VertexId, (String, List[Double], Int, String, String)), Int] = Graph(stationNodes, tripVertices)
+var graph: Graph[(String, List[Double], Int, String, String), Long] = Graph(stationNodes, tripVertices)
 graph.cache()
 
 
-// graph.
-// vertices.
-// filter { case (id, (name, age)) => age > 30 }.
-// collect.
-// foreach { case (id, (name, age)) => println(s"$name is $age")}
 
-// for (triplet <- graph.triplets.collect) {
-//     println(s"${triplet.srcAttr._1} likes ${triplet.dstAttr._1}")
-// }
+for (triplet <- graph.triplets.collect) {
+    println(s"FROM ${triplet.srcAttr._1} TO ${triplet.dstAttr._1} && ... ${triplet.dstAttr._4}")
+}
+
+graph.vertices.count // Nodes
+
+graph.edges.count  // Vertices
+
+graph.
+vertices.
+filter {
+    case (id, (name, gps, dockCount, city, date)) => city == "Redwood City"
+}.
+foreach {
+    case (id, (name, gps, dockCount, city, date)) => println(s"City : $city || stop : $name")
+}
+
+
+
+
+
+
+
 
 
